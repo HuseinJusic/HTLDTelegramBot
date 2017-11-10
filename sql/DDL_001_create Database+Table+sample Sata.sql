e5e27f368a923cadc09248d743dac9d49008a544
Moritz Bastigkeit <moritz.bastigkeit@hotmail.com>
2017-11-10 10:30:33 +0100

    msg

--- sql/DDL_001_create Database+Table+sample Sata.sql
+++ sql/DDL_001_create Database+Table+sample Sata.sql
@@ -13,16 +13,26 @@ with grant option;
 
 drop table if exists userdata;
 create table userdata(
-chat_id long not null,
+chat_id bigint not null,
 vname Varchar(20),
 nname Varchar(20),
 klass varchar(4),
-dstation varchar(50),
 
-KEY ix_leength_chat_id(chat_id(255))
+Primary Key (chat_id)
+#KEY ix_leength_chat_id(chat_id(255))
 );
 
-insert into userdata (chat_id,vname,nname,klass,dstation) values (234567765,'husein','jusic','4cWI','Rheinstraße');
-
+insert into userdata (chat_id,vname,nname,klass) values (234567765,'husein','jusic','4cWI');
 
 select * from userdata WHERE chat_id =234567765;
+
+drop table if exists station;
+create table station(
+s_id int not null AUTO_INCREMENT,
+chat_id bigint not null,
+dstation varchar(50),
+slink varchar(100),
+
+Primary Key (s_id),
+FOREIGN KEY (chat_id) REFERENCES userdata(chat_id)
+);
